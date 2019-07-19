8bf00db12bb7cb00c466d212b3623ae4b0f298ac
Peter Nguyen <ptnguyen1987@gmail.com>
2019-07-19 19:04:58 +0700

    update intro

--- dev_portfolio/template-parts/frontpage-contact.php
+++ dev_portfolio/template-parts/frontpage-contact.php
@@ -1,8 +1,8 @@
-<div class="contact__section">
+<div id="contact" class="contact__section">
   <div class="contact__container container">
     <h2 class="contact__header bottom margin medium">Want to collaborate on something?</h2>
     <p class="contact__message">Send me a message at <span class="contact__email"><a
           href="mailto:hi@meetpeter.us?Subject=Hello!" target="_top">hi@meetpeter.us</a></span></p>
     <div class="code__tooltip contact__emailTooltip tooltip up blue" onClick="codeModal.show()">Email button</div>
-
   </div>
+</div>
--- dev_portfolio/template-parts/frontpage-intro.php
+++ dev_portfolio/template-parts/frontpage-intro.php
@@ -2,18 +2,23 @@
   <div class="intro__container container">
     <div class="content">
       <h1 class="intro__header">Hi. I am Peter.</h1>
-      <p class="intro__paragraph medium margin bottom">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-        Accusamus
-        dolores inventore
-        voluptates magnam architecto recusandae. Totam reprehenderit magnam repellat ipsam quam dolore quisquam quod
-        esse
-        animi eaque! Necessitatibus consequatur at cumque tempora? Beatae voluptate quam ipsam commodi eius earum at.
-        Asperiores ipsa maxime dignissimos nobis quas blanditiis ducimus praesentium animi.</p>
-      <p class="intro__paragraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi deleniti dolorum eum
-        eligendi laboriosam inventore ullam iste voluptatem numquam asperiores blanditiis excepturi et, hic vitae? Ea
-        quis
-        voluptate sapiente facere ipsa dolorem placeat consequatur vel iste blanditiis unde architecto, asperiores
-        veritatis? Dignissimos, fuga sit nobis soluta enim provident distinctio non?</p>
+      <p class="intro__paragraph small margin bottom">And I'm a problem solver.</p>
+      <p class="intro__paragraph small margin bottom">For the past 10 years, whether working
+        in universities in the US or with NGOs in Southeast Asia, I have always focused on what I can do to address the
+        challenges my organizations or my communities faced. My driving question became "what problem do I want to solve
+        in the world?"</p>
+      <p class="intro__paragraph small margin bottom">I started coding, because I was curious. FreeCodeCamp popped onto
+        my radar and I powered through the front end certification within 4 weeks. Months later, I had completed CS50,
+        read a few You Don't Know JS books, and went through some Udemy courses. I was obsessed with learning more, and
+        it was all so fun. It
+        was probably somewhere in the middle of the night, after already a 10 hour day of coding, still excited about
+        some problem I was trying to solve when I really began to think, maybe this is my next challenge?
+      </p>
+      <p class="intro__paragraph small margin bottom">I'm somewhat of an idealist and I hope to figure out how I can use
+        what I've learned to make the world better.</p>
+      <a href="#contact">
+        <p class="intro__paragraph small margin bottom">Let's change the world.</p>
+      </a>
     </div>
 
     <div class="code__tooltip intro__tooltip tooltip left blue" onClick="codeModal.show(1807)">CSS Philosophy</div>
